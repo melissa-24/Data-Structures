b50edc6748aab2d453636ac6bea215ca269d86fd
Melissa Longenberger <melissalongenberger24@gmail.com>
2020-09-22 20:55:29 -0400

    stack and singly linked

--- stack/stack.py
+++ stack/stack.py
@@ -12,14 +12,38 @@ return elements in Last In First Out order.
 """
 class Stack:
     def __init__(self):
-        self.size = 0
-        # self.storage = ?
+        self.storage = []
 
     def __len__(self):
-        pass
+        return len(self.storage)
 
     def push(self, value):
-        pass
+        self.storage.append(value)
 
     def pop(self):
-        pass
+        if len(self.storage) == 0:
+            return None
+        return self.storage.pop()
+
+
+import sys
+sys.path.insert(1, '../singly_linked_list/')
+from singly_linked_list import LinkedList
+
+class StackLinked:
+    def __init__(self):
+        self.size = 0
+        self.storage = LinkedList()
+
+    def __len__(self):
+        return self.size
+
+    def push(self, value):
+        self.storage.add_to_head(value)
+        self.size += 1
+
+    def pop(self):
+        if self.size == 0:
+            return None
+        self.size -= 1
+        return self.storage.remove_head()
