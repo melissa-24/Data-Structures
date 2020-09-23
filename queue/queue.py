765952ed395d99e1a7d7917751e9fb363576cf76
Melissa Longenberger <melissalongenberger24@gmail.com>
2020-09-22 20:57:31 -0400

    queue done

--- queue/queue.py
+++ queue/queue.py
@@ -13,6 +13,7 @@ return elements in First In First Out order.
 Stretch: What if you could only use instances of your Stack class to implement the Queue?
          What would that look like? How many Stacks would you need? Try it!
 """
+
 class Queue:
     def __init__(self):
         self.storage = []
@@ -27,3 +28,25 @@ class Queue:
         if len(self.storage) == 0:
             return None
         return self.storage.pop(0)
+
+import sys
+sys.path.insert(1, '../singly_linked_list/')
+from singly_linked_list import LinkedList
+
+class QueueLinked:
+   def __init__(self):
+      self.size = 0
+      self.storage = LinkedList()
+   
+   def __len__(self):
+      return self.size
+   
+   def enqueue(self, value):
+      self.size += 1
+      return self.storage.add_to_tail(value)
+   
+   def dequeue(self):
+      if self.size < 1:
+         return None
+      self.size -= 1
+      return self.storage.remove_head()
